cb82416f6a230f12fab036482e5178760ce3eed9
uran <kdm10ho@naver.com>
2026-08-03 10:55:39 +0900

    auto

--- auto_upload.bat
+++ auto_upload.bat
@@ -1,8 +1,5 @@
 git add .
-set comment=
-cls
-set /p comment=Enter Comment:
-if "%comment%" == "" comment="auto uploaded"
+set comment="auto uploaded"
 git commit -m "%comment%"
 git pull --rebase
 git push origin main
